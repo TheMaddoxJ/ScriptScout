5301ffbf048d948aecbeb71018cfbd5765c5214a
jhdavey <jhdavey5182@gmail.com>
2023-05-25 21:16:06 -0400

    Added connection.js, user controller, user model, user routes

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3001
-    }
-  );
-}
+const sequelize = new Sequelize(
+  'scriptscout_db',
+  'root',
+  'password',
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+  }
+);
 
 module.exports = sequelize;
