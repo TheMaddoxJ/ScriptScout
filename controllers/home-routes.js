69e8ea69a385014139803d7b2873bdc0b10df275
jhdavey <jhdavey5182@gmail.com>
2023-05-26 22:26:01 -0400

    worked more on setting up login routes and views

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,18 +6,13 @@ router.get('/', (req, res) => {
   res.render('homepage');
 });
 
-
-
-
-
-// COMMENTED OUT FOR NOW TO INPUT JOB LIST ON HOMEPAGE
-// // Login route
-// router.get('/login', (req, res) => {
-//   if (req.session.loggedIn) {
-//     res.redirect('/');
-//     return;
-//   }
-//   res.render('login');
-// });
+// Login route
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
 
 module.exports = router;
