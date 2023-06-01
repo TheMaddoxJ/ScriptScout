cc180f4f26a2945fc91b1a0557cfaa288bfe8ffa
jhdavey <jhdavey5182@gmail.com>
2023-06-01 16:34:18 -0400

    Added bootstrap cdn for styling, reworked main layout.handlebars, split login/signup forms into two seperate views, etc. STILL NEED TO GE T loggedIn conditions to work in views?

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,79 +2,64 @@ const router = require('express').Router();
 const { User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// CREATE new user
+// singup user ('/api/users)
 router.post('/', async (req, res) => {
-  try {
-    const dbUserData = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-
-    req.session.save(() => {
-      req.session.loggedIn = true;
-
-      res.status(200).json(dbUserData);
-      
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+    try {
+        const dbUserData = await User.create(req.body);
+        req.session.save(() => {
+            req.session.name = dbUserData.name;
+            req.session.email = dbUserData.email;
+            req.session.password = dbUserData.password;
+            req.session.loggedIn = true;
+            res.status(201).json({ message: `Account created for ${dbUserData.username}`});
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
-// Login
+// login user ('/api/users/login')
 router.post('/login', async (req, res) => {
-  try {
-    const dbUserData = await User.findOne({
-      where: {
-        email: req.body.email,
-      },
-    });
-
-    if (!dbUserData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password. Please try again!' });
-      return;
+    try {
+        const dbUserData = await User.findOne({
+            where: {email: req.body.email}
+        });
+        if (!dbUserData) {
+            res.status(400).json({ message: `User id ${req.body.email} is not valid.` });
+            return;
+        }
+        // check pw
+        const pwValidated = await dbUserData.checkPassword(req.body.password)
+        if (!pwValidated) {
+            res.status(400).json({ message: "Incorrect password!" });
+            return;
+        }
+        // create session and send response back
+        req.session.save(() => {
+            req.session.name = dbUserData.name;
+            req.session.email = dbUserData.email;
+            req.session.loggedIn = true;        
+        //send response to client
+        res.status(200).json({ message: "You are logged in!" });
+        });
+    } catch (err) {
+        res.status(400).json(err);
     }
-
-    const validPassword = await dbUserData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password. Please try again!' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.loggedIn = true;
-
-      res
-        .status(200)
-        .json({ user: dbUserData, message: 'You are now logged in!' });
-        console.log('You are now logged in!');
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
 });
 
 // logout user ('/api/users/logout')
 router.post('/logout', withAuth, async (req, res) => {
-  try {
-      if (req.session.loggedIn) {
-          const dbUserData = await req.session.destroy(() => {
-              res.status(204).end();
-          });
-      } else {
-          res.status(404).end();
-      }
-  } catch {
-      res.status(400).end();
-  }
+    try {
+        if (req.session.loggedIn) {
+            const dbUserData = await req.session.destroy(() => {
+                res.status(204).end();
+            });
+        } else {
+            res.status(404).end();
+        }
+    } catch {
+        res.status(400).end();
+    }
 });
 
 module.exports = router;
