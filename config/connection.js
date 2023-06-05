41ba2ce70f2d0f40244118d3fe96887e5469ffcc
khusil <khushilmanji@yahoo.com>
2023-06-05 18:19:10 -0400

    changes in connection file for server to connect

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const sequelize = new Sequelize(
   'scriptscout_db',
   'root',
-  'password',
+  'Mahee2017@',
   {
-    host: 'localhost',
+    host: '127.0.0.1',
     dialect: 'mysql',
     port: 3306,
   }
